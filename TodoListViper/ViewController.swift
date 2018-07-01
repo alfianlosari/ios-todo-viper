ab953b7e6ba6ebef353e44422825587e641bc466
Alfian Losari <alfianlosari@gmail.com>
2018-07-01 19:26:36 +0700

    Initial Commit

---
+++ TodoListViper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoListViper
+//
+//  Created by Alfian Losari on 7/1/18.
+//  Copyright © 2018 Alfian Losari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
